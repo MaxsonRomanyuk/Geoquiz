1ab2ffc72b6d307f464cf6129b125abe4965cff5
MaxsonRomanyuk <bdnya@inbox.ru>
2026-02-23 19:07:52 +0300

    feat: (PvP matchmaking) add SignalR DTOs and notification interface for real-time PvP

---
+++ backend/GeoQuiz_backend/GeoQuiz_backend/Application/Interfaces/ISignalRNotificationService.cs
@@ -0,0 +1,21 @@
+﻿using GeoQuiz_backend.DTOs.PvP;
+
+namespace GeoQuiz_backend.Application.Interfaces
+{
+    public interface ISignalRNotificationService
+    {
+        Task NotifyMatchFound(Guid userId, MatchFoundWithDraftData matchData);
+
+        Task NotifyDraftUpdated(Guid matchId, DraftUpdateData updateData);
+
+        Task NotifyGameReady(Guid matchId, GameReadyData gameData);
+
+        Task NotifyQuestionResult(Guid userId, QuestionResultData resultData);
+
+        Task NotifyTimerUpdate(Guid matchId, TimerUpdateData timerData);
+
+        Task NotifyGameFinished(Guid userId, GameFinishedData finishData);
+
+        Task NotifyOpponentDisconnected(Guid userId, DisconnectData disconnectData);
+    }
+}
